772c61cec1280bff43b58fdaf945f48252b1a4e2
Lolloroma114 <83644220+Lolloroma114@users.noreply.github.com>
2021-05-29 17:14:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myhealth/CondivisioneMisure.java
@@ -0,0 +1,23 @@
+package com.example.myhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class CondivisioneMisure extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_condivisione_misure);
+    }
+
+    public void condivisioneDatabase(View v){
+        Context context = getApplication();
+        Toast toast = Toast.makeText(context, "Condivido sul Database", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
